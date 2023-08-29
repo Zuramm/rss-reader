894f7df734abf81672f4f37b90a81b92c71e274b
Zuramm <opheder@gmail.com>
2023-08-29 21:18:05 +0200

    add layout

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/template/html/v2"
 )
 
@@ -16,9 +18,15 @@ func main() {
 	engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{
-		Views: engine,
+		Views:       engine,
+		ViewsLayout: "layouts/main",
 	})
 
+	// Middleware
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	// Routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", nil)
 	})
